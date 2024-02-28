445f5f3a6cf5a1e2c2c25e41574e88ffbbbd4871
LUIZ CARLOS ESTEVAO CASTRO <202302783601@alunos.estacio.br>
2024-02-28 10:06:02 -0300

    atualizacoes

---
+++ main.py
@@ -0,0 +1,59 @@
+# arquivo = open("texto.txt", "r") # Abre o arquivo "texto.txt"
+
+# print(arquivo.readline()) # Ler uma linha 
+# print(arquivo.readline()) # Ler outra linha
+# print(arquivo.seek(0)) # Coloca o cursor na posicao 0 do arquivo
+# print(arquivo.readlines()) # Ler todas as linhas, junto com as quebras (\n)
+# print(arquivo.seek(0)) 
+
+# print(arquivo.read()) # ler o arquivo todo
+# print(arquivo.tell()) # Retorna a quantidade de caracteres
+
+# arquivo.close() # Fecha o arquivo
+# print(arquivo.closed)
+
+# print(arquivo.read())
+# print(arquivo.seek(0))
+# print(arqivo.read(8))
+
+# print(arquivo.name)
+# print(arquivo.mode)
+# print(arquivo.mode)
+# print(arquivo.closed)
+
+# arquivo = open("novo.txt", "w") # Cria um novo arquivo, se nao existir
+
+# arquivo.write("Arquivo de escrita") # Escreve no arquivo
+
+# arquivo.close() # Fecha o arquivo
+# print(arquivo.closed) # Retorna se o arquivo ta fechado ou nao ( True ou False )
+
+# arquivo = open("frutas.txt", "w")
+# arquivo.write("Banana" + "\n")
+# arquivo.write("Uva" + "\n")
+# arquivo.write("Mamao" + "\n")
+
+# arquivo.close()
+
+# precos = [20, 100, 500, 600]
+
+# with open("precos roupas.txt", "w") as arquivo: # Cria um arquivo e ao sair do with, fecha automaticamente o arquivo
+#     for preco in precos:
+#         arquivo.write(str(preco) + "\n")
+
+# print(arquivo.closed)
+
+# precos = [800]
+
+# with open("precos roupas.txt", "a") as arquivo:
+#     for preco in precos:
+#         arquivo.write(str(preco) + "\n")
+
+
+
+
+
+
+
+
+
